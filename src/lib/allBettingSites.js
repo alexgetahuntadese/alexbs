98b456905b70392a9b52a5437167bf291e8e20d7
alexgetahuntadese <alexgetahuntadese@gmail.com>
2025-05-26 23:58:41 +0300

    Update allBettingSites.js

--- src/lib/allBettingSites.js
+++ src/lib/allBettingSites.js
@@ -50,11 +50,6 @@ export const bettingSites = [
     url: "https://www.winneret.com"
   },
   {
-    name: "Utop",
-    bonus: "100% First Deposit Bonus",
-    url: "https://www.utop.com"
-  },
-  {
     name: "Fidel Sport",
     bonus: "Bonus N/A",
     url: "https://www.fidelsport.com/?referal=77891"
@@ -75,11 +70,6 @@ export const bettingSites = [
     url: "https://www.axumbet.com"
   },
   {
-    name: "GalaxyBetting",
-    bonus: "Bonus N/A",
-    url: "https://www.galaxybetting.com"
-  },
-  {
     name: "Vamos Bet",
     bonus: "300% Multi Bonus on Win",
     url: "https://www.vamosbet.com"
@@ -95,59 +85,9 @@ export const bettingSites = [
     url: "https://www.harifsport.com?referal=350278"
   },
   {
-    name: "BetKing",
-    bonus: "225% Accumulator Bonus",
-    url: "https://www.betking.com.et"
-  },
-  {
     name: "Waliyabet",
     bonus: "15 ETB Welcome Bonus",
     url: "http://waliyabet.et/register/?referal=784301"
-  },
-  {
-    name: "Bet251",
-    bonus: "100% Bonus up to 5,000 ETB",
-    url: "https://www.bet251.com"
-  },
-  {
-    name: "Habeshabets",
-    bonus: "300% Multi Bonus",
-    url: "https://www.habeshabets.com"
-  },
-  {
-    name: "Afro Betting",
-    bonus: "Multi Bonus up to 50,000 ETB",
-    url: "https://www.afrobetting.com"
-  },
-  {
-    name: "Sheger Bet",
-    bonus: "Bonus N/A",
-    url: "https://www.shegerbet.com"
-  },
-  {
-    name: "Qwickbirr",
-    bonus: "10 Free Spins for the First Deposit",
-    url: "https://qwickbirr.com/?aff=233019"
-  },
-  {
-    name: "BetX",
-    bonus: "10 Free Bets for the First Deposit",
-    url: "https://www.betx.et"
-  },
-  {
-    name: "Arada Bet",
-    bonus: "300% Multi Bonus on Win",
-    url: "https://www.aradabet.com"
-  },
-  {
-    name: "Best Bet",
-    bonus: "Accumulator Bonus up to 400%",
-    url: "https://www.bestbet.com"
-  },
-  {
-    name: "Blackjack Bet",
-    bonus: "50% Cash Back Bonus",
-    url: "https://www.blackjackbet.com"
   },
   {
     name: "ZemenBet",
@@ -158,36 +98,6 @@ export const bettingSites = [
     name: "Ethiobet",
     bonus: "200% Multi Bonus",
     url: "https://www.ethiobet.et"
-  },
-  {
-    name: "GrandBet",
-    bonus: "Up to 10,000 ETB Free Bets",
-    url: "https://www.grandbet.com"
-  },
-  {
-    name: "DashBet",
-    bonus: "Weekly Cashback Offers",
-    url: "https://www.dashbet.com"
-  },
-  {
-    name: "GalaxyWin",
-    bonus: "500% Accumulator Bonus",
-    url: "https://www.galaxywin.com"
-  },
-  {
-    name: "Winner Ethiopia",
-    bonus: "100% First Deposit Bonus",
-    url: "https://www.winnerethiopia.com"
-  },
-  {
-    name: "VamosBet",
-    bonus: "400% Multi Betting Bonus",
-    url: "https://www.vamosbet.com"
-  },
-  {
-    name: "ElephantBet",
-    bonus: "Free Bets up to 5,000 ETB",
-    url: "https://www.elephantbet.com"
   },
   {
     name: "ChampionBet",
